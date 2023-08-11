141e7994a540b6241252620fe7e8f41d05325215
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-11 20:23:40 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paginations.apps.PaginationsConfig'
+    # 'paginations.apps.PaginationsConfig',
+    # 'models.apps.ModelsConfig'
+    # 'forms.apps.FormsConfig'
+    'todo.apps.TodoConfig'
 ]
 
 MIDDLEWARE = [
