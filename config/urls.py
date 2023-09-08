d7c933b0b5b73a08461b463e41a390ba66654fa6
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-09-08 19:38:54 +0500

    done

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # path('', include('forms.urls'))
     # path('', include('todo.urls'))
     # path('', include('paginator.urls'))
-    path('', include('user.urls'))
+    # path('', include('user.urls'))
+    path('', include('file.urls'))
+
 
 ]
