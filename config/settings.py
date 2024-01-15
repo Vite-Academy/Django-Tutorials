198f3c6bada1f34e4ad8908db0cb80ad7ac0e3a5
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-01-15 18:54:56 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -20,13 +20,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'jazzmin',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'base.apps.BaseConfig'
     # 'django_cleanup.apps.CleanupConfig',
     'todoapps.apps.TodoappsConfig',
 ]
