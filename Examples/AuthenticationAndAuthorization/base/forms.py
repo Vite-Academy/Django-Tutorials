da2dd8cc57e024dfa742af3317ed2b9281d2a11e
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-02-10 17:01:27 +0500

    done

--- Examples/AuthenticationAndAuthorization/base/forms.py
+++ Examples/AuthenticationAndAuthorization/base/forms.py
@@ -1,18 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import *
-
-
-# import the standard Django Forms 
-# from built-in library 
-from django import forms  
+from .models import * 
     
 # creating a form   
-class InputForm(forms.Form):  
-    
-    first_name = forms.CharField(max_length = 200)  
-    last_name = forms.CharField(max_length = 200)  
-    roll_number = forms.IntegerField(  
-                     help_text = "Enter 6 digit roll number"
-                     )  
-    password = forms.CharField(widget = forms.PasswordInput())
+
+# class InputForm(forms.Form):  
+#     first_name = forms.CharField(max_length = 200)  
+#     last_name = forms.CharField(max_length = 200)  
+#     roll_number = forms.IntegerField(  
+#                      help_text = "Enter 6 digit roll number"
+#                      )  
+#     password = forms.CharField(widget = forms.PasswordInput())
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        # fields = ('title', 'text',)
+        fields = "__all__"
--- Examples/AuthenticationAndAuthorization/base/views.py
+++ Examples/AuthenticationAndAuthorization/base/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.http import HttpResponseRedirect
-
+import time
 from .forms import *
   
 # Create your views here. 
 
-def home_view(request): 
-    context ={} 
-    context['form']= InputForm() 
-    return render(request, "pages/home.html", context)
+# def home_view(request): 
+#     context ={} 
+#     context['form']= InputForm() 
+#     return render(request, "pages/home.html", context)
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, "pages/home.html", {'form': form})
