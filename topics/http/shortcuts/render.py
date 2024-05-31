b96c1f5afb1b05cdd46ddabb07d57179346d76e0
shakhzodtojiev <tojiyevshakhzod@gmail.com>
2024-06-01 00:22:18 +0500

    done

--- topics/http/shortcuts/render.py
+++ topics/http/shortcuts/render.py
@@ -4,4 +4,21 @@ from django.shortcuts import render
 
 def base(request):
     context = {'data': 200}
-    return render(request, 'base.html', context=context)
+    return render(
+        request, 'base.html', 
+        context=context, 
+        content_type='text/html', 
+        status=200, using=None
+        )
+
+
+# This example is equivalent to:
+
+# from django.http import HttpResponse
+# from django.template import loader
+
+# def my_view(request):
+#     # View code here...
+#     t = loader.get_template("base.html")
+#     c = {"foo": "bar"}
+#     return HttpResponse(t.render(c, request), content_type="application/xhtml+xml")
