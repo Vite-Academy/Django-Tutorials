71a92c0708a3608c9e795a985147a0a119a843fa
shakhzodtojiev <tojiyevshakhzod@gmail.com>
2024-05-31 23:55:49 +0500

    done

--- packages/django-ckeditor/base/views.py
+++ packages/django-ckeditor/base/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     context = {'data': 200}
-    return render(request, 'base.html', context=context)
+    return render(
+        request, 'base.html', 
+        context=context, 
+        content_type='text/html', 
+        status=200, using=None
+        )
