7e0b4c09136cd7c916ee7a0172dee70cd42eeffd
shakhzodtojiev <shakhzodtojievuz@gmail.com>
2024-08-06 17:45:54 +0500

    done

--- examples/forms/config/settings.py
+++ examples/forms/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'base.apps.BaseConfig'
+    # Packages
 ]
 
 MIDDLEWARE = [
