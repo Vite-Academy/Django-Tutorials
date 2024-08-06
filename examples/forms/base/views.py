e7ab2865992554f1d4a4487ba2fc3e0f042c6099
shakhzodtojiev <shakhzodtojievuz@gmail.com>
2024-08-06 17:41:35 +0500

    done

---
+++ examples/forms/base/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic import FormView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+
+# Create your views here.
+class HomeView(TemplateView):
+    template_name = "pages/home.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["company_name"] = "My Awesome Company"
+    #     context["description"] = "We provide amazing products and services."
+    #     return context
